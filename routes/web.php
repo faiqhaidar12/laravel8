04628084306261855c2a0df458f64f382cfe7771
faiqhaidar12 <faiqhaidar1@gmail.com>
2023-03-21 11:21:52 +0700

    third commit

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,37 @@ Route::get('/debug', function () {
 
     ddd($data);
 });
+
+$tugas = [
+    'pertama' => 'makan',
+    'kedua' => 'minum',
+    'ketiga' => 'tidur'
+];
+
+Route::get('/tugas', function () use ($tugas) {
+    if (request()->search) {
+        return $tugas[request()->search];
+    }
+
+    return $tugas;
+});
+
+Route::get('/tugas/{param}', function ($param) use ($tugas) {
+    return $tugas[$param];
+});
+
+Route::post('/tugas', function () use ($tugas) {
+    // return request()->all();
+    $tugas[request()->label] = request()->tugas;
+    return $tugas;
+});
+
+Route::patch('/tugas/{key}', function ($key) use ($tugas) {
+    $tugas[$key] = request()->tugas;
+    return $tugas;
+});
+
+Route::delete('/tugas/{key}', function ($key) use ($tugas) {
+    unset($tugas[$key]);
+    return $tugas;
+});
